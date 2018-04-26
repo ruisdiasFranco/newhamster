1f9760d2dbe6726b1c469ae9142e1ce0d57b066f
Ruis Dias, Franco <ruisdias.franco@gmail.com>
2018-04-26 15:44:15 -0300

    Test

--- actions.go
+++ actions.go
@@ -1,1 +1,67 @@
 package newhamster
+
+import (
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func productAdd(writer http.ResponseWriter, reader *http.Request){
+	decoder := json.NewDecoder(reader.Body)
+
+	var movie_data Movie
+	err := decoder.Decode(&movie_data)
+
+	if(err != nil){
+		panic(err)
+	}
+
+	defer reader.Body.Close()
+
+	err = collection.Insert(movie_data)
+
+	if err != nil{
+		writer.WriteHeader(500)
+		return
+	}
+
+	responseMovie(writer, 200, movie_data)
+}
+func producUpdate()
+
+func productRemove(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	movie_id := params["id"]
+
+	if !bson.IsObjectIdHex(movie_id) {
+		w.WriteHeader(404)
+		return
+	}
+
+	oid := bson.ObjectIdHex(movie_id)
+
+	decoder := json.NewDecoder(r.Body)
+
+	var movie_data Movie
+	err := decoder.Decode(&movie_data)
+
+	if err != nil {
+		panic(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	defer r.Body.Close()
+
+	document := bson.M{"_id": oid}
+	change := bson.M{"$set": movie_data}
+	err = collection.Update(document, change)
+
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	responseMovie(w, 200, movie_data)
+}
--- routes.go
+++ routes.go
@@ -1,1 +1,85 @@
 package newhamster
+import(
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+type Route struct{
+	Name string
+	Method string
+	Pattern string
+	HandleFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func newRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range routes{
+		router.Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandleFunc)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		index,
+	},
+	Route{
+		"productList",
+		"GET",
+		"/product",
+		productList,
+	},
+	Route{
+		"prodcutShow",
+		"GET",
+		"/product/{id}",
+		productShow,
+	},
+	Route{
+		"prodcutAdd",
+		"POST",
+		"/product",
+		productAdd,
+	},
+	Route{
+		"prodcutRemove",
+		"DELETE",
+		"/product",
+		productRemove,
+	},
+	Route{
+		"prodcutUpdate",
+		"PUT",
+		"/product/{id}",
+		productUpdate,
+	},
+	Route{
+		"shoppingCartList",
+		"GET",
+		"/shoppging-cart",
+		shoppingCartList,
+	},
+	Route{
+		"shoppingCartAddProduct",
+		"POST",
+		"/shoppging-cart",
+		shoppingCartAddProduct,
+	},
+	Route{
+		"shoppingCartRemove",
+		"DELETE",
+		"/shopping-cart/{id}",
+		shoppingCartAddProduct,
+	},
+
+}
